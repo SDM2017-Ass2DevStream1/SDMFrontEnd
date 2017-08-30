a040cfec43713735b18c332b289178d1859057bd
Nick <kiwi.sedna@gmail.com>
2017-08-31 07:32:55 +1200

    add style for search bar

--- src/containers/search_index/index.jsx
+++ src/containers/search_index/index.jsx
@@ -6,18 +6,38 @@ import * as searchActions from '../../actions/search';
 
 
 class SearchIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.onFormSubmit = this.onFormSubmit.bind(this);
+  }
+
+  onFormSubmit(e) {
+    e.preventDefault();
+    console.log(this.searchInput.value);
+  }
+
   render() {
     return (
-      <div>
-        <input type="text" placeholder="search" />
-      </div>
+      <form className="input-group" onSubmit={this.onFormSubmit}>
+        <input
+          type="text"
+          ref={(input) => { this.searchInput = input; }}
+          className="form-control"
+          placeholder="Input article title or author names"
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-outline-primary">
+            Search
+          </button>
+        </span>
+      </form>
     );
   }
 }
 
-const mapStateToProps = state => ({
-  search: state.search,
-});
+const mapStateToProps = ({ search }) => {
+  return { search };
+};
 
 const mapDispatchToProps = dispatch => ({
   actions: bindActionCreators(searchActions, dispatch),
