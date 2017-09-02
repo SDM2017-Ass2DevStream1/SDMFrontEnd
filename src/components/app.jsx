b0998ec2ce823b0dae84f3ded34be4cf1669b449
Nick <kiwi.sedna@gmail.com>
2017-09-02 12:24:04 +1200

    Use redux router

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import SearchIndex from '../containers/search_index';
 
 
 export default () => (
-  <MuiThemeProvider>
-    <div>
-      <header>Header</header>
-      <section id="content">
-        <SearchIndex />
-      </section>
-    </div>
-  </MuiThemeProvider>
+  <BrowserRouter>
+    <MuiThemeProvider>
+      <div>
+        <header>Header</header>
+        <section id="content">
+          <Switch>
+            <Route exact path="/" component={SearchIndex} />
+          </Switch>
+        </section>
+      </div>
+    </MuiThemeProvider>
+  </BrowserRouter>
 );
