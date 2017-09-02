e3b0f04b4c7574e6bcd5175de7b435241165c6d6
Nick <kiwi.sedna@gmail.com>
2017-09-02 22:06:27 +1200

    add tab component

--- src/containers/search_index.jsx
+++ src/containers/search_index.jsx
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { Tabs, Tab } from 'material-ui/Tabs';
 
 import SearchResult from './search_result';
 import * as searchActions from '../actions/search';
@@ -22,22 +23,31 @@ class SearchIndex extends Component {
     const { search: { items } } = this.props;
 
     return (
-      <div>
-        <form className="input-group" onSubmit={this.onFormSubmit}>
-          <input
-            type="text"
-            ref={(input) => { this.searchInput = input; }}
-            className="form-control"
-            placeholder="Input article title or author names"
-          />
-          <span className="input-group-btn">
-            <button type="submit" className="btn btn-outline-secondary">
-              Search
-            </button>
-          </span>
-        </form>
-        {!_.isEmpty(items) && <SearchResult />}
-      </div>
+      <Tabs>
+        <Tab label="Search">
+          <div>
+            <form className="input-group" onSubmit={this.onFormSubmit}>
+              <input
+                type="text"
+                ref={(input) => { this.searchInput = input; }}
+                className="form-control"
+                placeholder="Input article title or author names"
+              />
+              <span className="input-group-btn">
+                <button type="submit" className="btn btn-outline-secondary">
+                  Search
+                </button>
+              </span>
+            </form>
+            {!_.isEmpty(items) && <SearchResult />}
+          </div>
+        </Tab>
+        <Tab label="History">
+          <div>
+            History
+          </div>
+        </Tab>
+      </Tabs>
     );
   }
 }
