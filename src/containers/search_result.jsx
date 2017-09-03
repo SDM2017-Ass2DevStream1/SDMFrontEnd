ee407ddee9f3a1ac9052034b99b08a6f43f66693
Nick <kiwi.sedna@gmail.com>
2017-09-03 12:23:38 +1200

    update table style

--- src/containers/search_result.jsx
+++ src/containers/search_result.jsx
@@ -1,35 +1,112 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {
+  Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn,
+} from 'material-ui/Table';
 import styled from 'styled-components';
 
+
+const flex = {
+  title: 3,
+  year: 1,
+  rating: 1,
+};
+
+const styles = {
+  row: {
+    display: 'flex',
+  },
+  column: {
+    display: 'flex',
+    alignItems: 'center',
+    // https://stackoverflow.com/questions/26465745/ellipsis-in-flexbox-container
+    minWidth: 0,
+  },
+};
+
+const ColumnContent = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
 
 class SearchResult extends Component {
   renderItems() {
     const { search: { items } } = this.props;
-    const Li = styled.li`
-      background: yellow;
-    `;
-    const Title = styled.h3`
-      font-size: 16px;
-      color: blue;
-    `;
-    const Description = styled.p`
-      color: red;
-    `;
 
     return items.map((item) => {
       return (
-        <Li key={item.id} className="item {styles.item}">
-          <Title>{item.title}</Title>
-          <Description>{item.desc}</Description>
-        </Li>
+        <TableRow key={item.id} style={styles.row}>
+          <TableRowColumn
+            style={{
+              ...styles.column,
+              flex: flex.title,
+            }}
+          >
+            <ColumnContent>
+              {item.title}
+            </ColumnContent>
+          </TableRowColumn>
+          <TableRowColumn
+            style={{
+              ...styles.column,
+              flex: flex.year,
+            }}
+          >
+            <ColumnContent>
+              {item.desc}
+            </ColumnContent>
+          </TableRowColumn>
+          <TableRowColumn
+            style={{
+              ...styles.column,
+              flex: flex.rating,
+            }}
+          >
+            <ColumnContent>
+              {item.desc}
+            </ColumnContent>
+          </TableRowColumn>
+        </TableRow>
       );
     });
   }
 
   render() {
     return (
-      <ul>{this.renderItems()}</ul>
+      <Table>
+        <TableHeader displaySelectAll={false}>
+          <TableRow style={styles.row}>
+            <TableHeaderColumn
+              style={{
+                ...styles.rowColumn,
+                flex: flex.title,
+              }}
+            >
+              Title
+            </TableHeaderColumn>
+            <TableHeaderColumn
+              style={{
+                ...styles.rowColumn,
+                flex: flex.year,
+              }}
+            >
+              Publish Year
+            </TableHeaderColumn>
+            <TableHeaderColumn
+              style={{
+                ...styles.rowColumn,
+                flex: flex.rating,
+              }}
+            >
+              Rating
+            </TableHeaderColumn>
+          </TableRow>
+        </TableHeader>
+        <TableBody displayRowCheckbox={false} stripedRows>
+          {this.renderItems()}
+        </TableBody>
+      </Table>
     );
   }
 }
