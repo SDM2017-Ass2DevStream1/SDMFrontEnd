078908fc312ab4c6939bb21828af5ce4f84cface
Nick <kiwi.sedna@gmail.com>
2017-09-06 13:10:01 +1200

    refactor style consts

--- src/constants/styles.js
+++ src/constants/styles.js
@@ -1,4 +1,15 @@
-import { colors } from 'material-ui/styles';
+import { getMuiTheme, colors } from 'material-ui/styles';
 
 
-export const BORDER = `1px solid ${colors.grey300}`;
+// This replaces the textColor value on the palette
+// and then update the keys for each component that depends on it.
+// More on Colors: http://www.material-ui.com/#/customization/colors
+export const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: colors.lightGreen500,
+    primary2Color: colors.lightGreen700,
+    pickerHeaderColor: colors.lightGreen500,
+  },
+});
+
+export const BORDER = `1px solid ${muiTheme.palette.borderColor}`;
