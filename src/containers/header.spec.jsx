40bfd1f208a555c8ed4d193b7873ea544676c956
Nick <kiwi.sedna@gmail.com>
2017-09-08 06:43:49 +1200

    stage

--- src/containers/header.spec.jsx
+++ src/containers/header.spec.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import PropTypes from 'prop-types';
+import { shallow } from 'enzyme';
+import configureStore from 'redux-mock-store';
 
 import Header from './header';
+import { muiTheme } from '../constants/styles';
 
 
 describe('Header container', () => {
+  const mockStore = configureStore();
+  let store;
+  let wrapper;
+
+  beforeEach(() => {
+    store = mockStore();
+    wrapper = shallow(<Header store={store} />, {
+      context: { muiTheme },
+      childContextTypes: { muiTheme: PropTypes.object },
+    });
+  });
+
   it('should display brand name', () => {
-    expect(true).toBe(true);
-    // const wrapper = mount(<Header />);
-    // expect(wrapper.find('#logo').text()).toContain('SERLER');
+    expect(wrapper.test()).to.equal('dd');
   });
 });
