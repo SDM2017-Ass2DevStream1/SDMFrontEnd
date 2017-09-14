bf41d79ff2dfea298e908484e98a2005473a6fd9
Nick <kiwi.sedna@gmail.com>
2017-09-15 09:55:36 +1200

    refactoring for exporting and importing

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { MuiThemeProvider } from 'material-ui/styles';
 
 import { muiTheme } from '../constants/styles';
-import Header from '../containers/header';
-import SearchIndex from '../containers/search_index';
+import { Header, SearchIndex } from '../containers';
 
 
 const App = () => (
