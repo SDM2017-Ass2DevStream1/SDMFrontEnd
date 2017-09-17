b03a978b0a37a863e98970fe1207fc5b9b5d2d31
Nick <kiwi.sedna@gmail.com>
2017-09-18 11:45:06 +1200

    fix action calls

--- src/containers/search_result.jsx
+++ src/containers/search_result.jsx
@@ -121,9 +121,14 @@ class SearchResult extends Component {
     }
 
     this.props.actions.sortSearchResultsBy(sortBy);
-    this.props.actions.fetchArticles({
-      ...this.props.search.query,
-    });
+
+    // HACK: to confirm that sortSearchResultsBy has already updated
+    // the query props
+    _.delay(() => {
+      this.props.actions.fetchArticles({
+        ...this.props.search.query,
+      });
+    }, 0);
   }
 
   renderVisibility() {
