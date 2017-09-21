544b0d58780eb5bef3759cf8d7632977f178ce60
Nick <kiwi.sedna@gmail.com>
2017-09-21 17:02:04 +1200

    page structure for categories

--- src/containers/categories.jsx
+++ src/containers/categories.jsx
@@ -1,14 +1,67 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+import {
+  RESEARCH_DESIGN, SE_METHOD, SE_METHODOLOGY,
+} from '../constants';
+import { ModuleTitle } from '../components/misc';
+import styled from 'styled-components';
 import * as searchActions from '../actions/search';
 
 
+const listData = {
+  design: {
+    title: 'Research Design',
+    items: RESEARCH_DESIGN,
+  },
+  method: {
+    title: 'SE Method',
+    items: SE_METHOD,
+  },
+  methodology: {
+    title: 'SE Methodology',
+    items: SE_METHODOLOGY,
+  },
+}
+
 class Categories extends Component {
+  renderItems(data) {
+    const Container = styled.div`
+      margin-bottom: 30px;
+    `;
+
+    const List = styled.ul`
+      font-size: 14px;
+      line-height: 2;
+      margin-left: -25px;
+    `;
+
+    const Item = styled.li`
+      display: inline-block;
+      margin-left: 25px;
+    `;
+
+    return (
+      <Container>
+        <ModuleTitle>{data.title}</ModuleTitle>
+        <List>
+          {_.values(data.items).map((item, i) => {
+            return <Item key={i}>{item}</Item>;
+          })}
+        </List>
+      </Container>
+    );
+  }
+
   render() {
     return (
-      <div>Categories</div>
+      <div>
+        {this.renderItems(listData.design)}
+        {this.renderItems(listData.method)}
+        {this.renderItems(listData.methodology)}
+      </div>
     );
   }
 }
